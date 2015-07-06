3774e6a10f5220d96d1a1c1ec524e93bc9b902fd
willson556 <thomas.willson@me.com>
2015-07-06 15:09:58 -0700

    Use NI SCM for calibration information.

---
+++ SCM Settings.scm
@@ -0,0 +1,27 @@
+[Paths]
+TargetAddress = "0.0.0.0"
+HostVI = "SCM Host Calibration.vi"
+PairingFile = "SCMPairing.par"
+UnitsFile = ""
+SnapshotSpecFile = ""
+SnapshotOutputFile = ""
+TARGET_VI = ""
+Project-Target-VI = ""
+WorkspaceFile = ""
+
+[Options]
+TRANSPARENT_MODE = FALSE
+MINI = FALSE
+AUTO = TRUE
+START_TARGET = FALSE
+FTP_VIS = FALSE
+SHOW_HOST_MENUS = FALSE
+CommLoopTime = 50
+FaultUpdateRate = 50
+
+[Faults]
+VeryLow = ""
+Low = ""
+Medium = ""
+High = ""
+VeryHigh = ""
